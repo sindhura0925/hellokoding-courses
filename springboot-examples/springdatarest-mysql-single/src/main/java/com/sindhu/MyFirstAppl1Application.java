3f182186fa4759db24f950afed29640875d1b190
Sindhura <sindhura0925@gmail.com>
2024-01-24 16:21:18 -0600

    adding product crud

---
+++ springboot-examples/springdatarest-mysql-single/src/main/java/com/sindhu/MyFirstAppl1Application.java
@@ -0,0 +1,13 @@
+package com.sindhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstAppl1Application.class, args);
+	}
+
+}
